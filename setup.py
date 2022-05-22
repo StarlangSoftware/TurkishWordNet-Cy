d88c2d8ef0e7e08bcc403658138a552bb2c4e9a5
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 22:37:26 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["WordNet/*.pyx", "WordNet/Similarity/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-WordNet-Cy',
-    version='1.0.7',
+    version='1.0.8',
     packages=['WordNet', 'WordNet.Similarity', 'WordNet.data'],
     package_data={'WordNet': ['*.pxd', '*.pyx', '*.c', '*.py'],
                   'WordNet.Similarity': ['*.pxd', '*.pyx', '*.c'],
